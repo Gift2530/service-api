77ec7e92e95af8863738e06e9958e4a5b315eb5f
Gift <Gift@GIFT>
2024-08-11 19:39:27 +0700

    fgfgf

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -51,56 +51,5 @@ namespace employee.Controllers
             }
             return NotFound();
         }
-
-        #region old
-        //[HttpGet("{id}")]
-        //public async Task<IActionResult> GetEmployee(int id)
-        //{
-        //    var employee = await _employeeRepository.GetByIdAsync(id);
-        //    if (employee == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return Ok(employee);
-        //}
-
-        //[HttpPost]
-        //public async Task<IActionResult> AddEmployee([FromBody] Employee employee)
-        //{
-        //    var rowsAffected = await _employeeRepository.AddAsync(employee);
-        //    if (rowsAffected > 0)
-        //    {
-        //        return Created("", 201);
-        //    }
-        //    return BadRequest("Failed to add employee");
-        //}
-
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> UpdateEmployee(int id, [FromBody] Employee employee)
-        //{
-        //    if (id != employee.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    var rowsAffected = await _employeeRepository.UpdateAsync(employee);
-        //    if (rowsAffected > 0)
-        //    {
-        //        return NoContent();
-        //    }
-        //    return NotFound();
-        //}
-
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteEmployee(int id)
-        //{
-        //    var rowsAffected = await _employeeRepository.DeleteAsync(id);
-        //    if (rowsAffected > 0)
-        //    {
-        //        return NoContent();
-        //    }
-        //    return NotFound();
-        //}
-        #endregion
     }
 }
